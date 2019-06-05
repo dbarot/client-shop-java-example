21a5088f85139d54edf9c52a22442afbe4c3fa03
dbarot <devarsh.barot@coveainsurance.co.uk>
2019-06-05 11:40:37 +0100

    Created Product and user entity models and created getters and setters.

---
+++ src/main/java/EntityModels/Product.java
@@ -0,0 +1,62 @@
+package EntityModels;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Product {
+
+    private UUID id;
+    private String name;
+    private String description;
+    private List<String> pictures;
+    private Integer pricePerUnit;
+    private List<String> tags;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<String> pictures) {
+        this.pictures = pictures;
+    }
+
+    public Integer getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public void setPricePerUnit(Integer pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+}
---
+++ src/main/java/EntityModels/User.java
@@ -0,0 +1,50 @@
+package EntityModels;
+
+public class User {
+
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String passwordHash;
+    private String address;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
